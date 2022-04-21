caad43ad0e6fc70c8a45c5202fe8cb1234505167
geerky42 <geerky42@gmail.com>
2022-04-20 22:40:15 -0600

    Added Solution and edited Remind

--- commands/help.js
+++ commands/help.js
@@ -3,6 +3,6 @@ module.exports = {
     description: 'prints out all possible commands',
 
     async execute(message, args, Discord, bot){
-        message.reply('Possible commands are:\n !submitLevel <levelname>\n !modmessage <message>\n !remind <world name> <level number>')
+        message.reply('Possible commands are:\n !submitLevel <levelname>\n !modmessage <message>\n !remind <world name> [level number]>\n !solution <world name> <level number>')
     }
 }
