3f555205f6e9ce2699d61771a10d88871f3b19ff
geerky42 <geerky42@gmail.com>
2022-05-09 22:13:08 -0600

    Improve the help command

--- commands/help.js
+++ commands/help.js
@@ -3,6 +3,11 @@ module.exports = {
     description: 'prints out all possible commands',
 
     async execute(message, args, Discord, bot){
-        message.reply('Possible commands are:\n !submitLevel <levelname>\n !modmessage <message>\n !remind <world name> [level number]\n !solution <world name> <level number>')
+        message.reply([
+            'Possible commands are:',
+            '!modmessage <message>',
+            '!remind <world name> [level number]',
+            '!solution <world name> <level number>'
+        ].join('\n\t'));
     }
 }
